47001e1cc2173561bcb3915563c18929cd7a1104
kenji.elzerman <kenslearningcurve@gmail.com>
2024-10-23 13:13:42 +0100

    Adding Chunks

---
+++ CSharpTipsAndTricks_102024/Chunks/Program.cs
@@ -0,0 +1,11 @@
+﻿using Logic;
+
+IEnumerable<Product[]> chunks = ProductList.Products.Chunk(3);
+
+foreach (Product[] chunk in chunks)
+{
+    foreach (Product product in chunk)
+    {
+        Console.WriteLine(product.Title);
+    }
+}
