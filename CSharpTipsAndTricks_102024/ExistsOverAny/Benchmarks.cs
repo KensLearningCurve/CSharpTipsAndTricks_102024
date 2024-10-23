3f46207aed7a20d373defd5fdb4f1e5ea720ce17
kenji.elzerman <kenslearningcurve@gmail.com>
2024-10-23 12:02:26 +0100

    Adding ExistsOverAny

---
+++ CSharpTipsAndTricks_102024/ExistsOverAny/Benchmarks.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using Logic;
+using System;
+using System.Linq;
+
+namespace ExistsOverAny;
+
+public class Benchmarks
+{
+    [Benchmark]
+    public void UsingAny()
+    {
+        string hasProductsBeingOrderedAny = ProductList.Products.Any(x => x.Status == Status.Ordered)
+            ? "There are products being ordered."
+            : "There are no products being orderd right now.";
+
+        Console.WriteLine(hasProductsBeingOrderedAny);
+    }
+
+    [Benchmark]
+    public void UsingExists()
+    {
+        string hasProductsBeingOrderedExists = ProductList.Products.Exists(x => x.Status == Status.Ordered)
+            ? "There are products being ordered."
+            : "There are no products being orderd right now.";
+
+        Console.WriteLine(hasProductsBeingOrderedExists);
+    }
+}
